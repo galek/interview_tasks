fdf3fbe5f7894f4de980334ec8d9d710e1f032c2
Nick Galko <galek@users.noreply.github.com>
2020-02-12 08:25:37 +0300

    Create chess.js

---
+++ chess.js
@@ -0,0 +1,29 @@
+/*
+Шахматная доска
+Задача вывести шахматную доску.
+TODO: Сделать нужно CSS, и вывести это визуально.
+
+Сейчас здесь схематичное решение.
+*/
+let array=[];
+
+function drawChess()
+{
+  let isBlack = true;
+    
+  for(let i=0;i<8;i++)
+  for(let j=0;j<8;j++)
+  {
+  if(j==0) isBlack!=isBlack;
+  
+  if(isBlack) {
+    array.push({isBlack:true, x:i, y:j});
+  }
+  else
+  {
+    array.push({isBlack:true, x:i, y:j});
+  }
+  
+  isBlack!=isBlack;
+  }
+}
