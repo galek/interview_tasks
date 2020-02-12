cb125e47852cfa885b61003e03b09096e804d610
Nick Galko <nikolay.galko@gmail.com>
2020-02-12 11:37:40 +0300

    Начинаю решать задачки с hackerRank

---
+++ hackerrank/cpp/cpp-hello-world.cpp
@@ -0,0 +1,14 @@
+/*
+Источник: hackerRank
+Задача: Напиши Hello, world! используя stdout или printf;
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	// printf("Hello, World!");
+    std::cout<<"Hello, World!";
+    return 0;
+}
---
+++ hackerrank/cpp/cpp-input-and-output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i, j, d;   
+	cin >> i >>j >>d;
+	cout<< i+j+d;
+    return 0;
+}
