3ed0007d7c188c91e462a24b6d1dcadbe4d8266d
Nikolay Galko <galek@users.noreply.github.com>
2021-10-27 01:39:00 +0300

    Create arrayToObj.js

---
+++ yandex/arrayToObj.js
@@ -0,0 +1,30 @@
+/* На входе массив
+var arr = [
+{name: 'width', value: 10},
+{name: 'height', value: 20}
+];
+//На выходе объект {width: 10, height: 20}
+*/
+
+const arr = [{
+    name: 'width',
+    value: 10
+  },
+  {
+    name: 'height',
+    value: 20
+  }
+];
+
+//{width: 10, height: 20}
+
+const saveObj = {}
+for (let tmp of arr) {
+  console.warn({
+    tmp
+  })
+
+  saveObj[tmp.name] = tmp.value
+}
+
+console.warn(saveObj)
