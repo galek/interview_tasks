cf0dcaa572a4e200740d089b7cfc7b35854031e4
Nikolay Galko <galek@users.noreply.github.com>
2021-10-27 02:44:36 +0300

    Create jscontext.js

---
+++ yandex/jscontext.js
@@ -0,0 +1,26 @@
+/*
+Задача на контексты - что тут будет выведено?
+*/
+var age = 10;
+var user = {
+  age: 20,
+  getAge: function() {
+    var age = 30;
+
+    return this.age;
+  }
+};
+
+
+console.warn("1: " + user.getAge()) // 20, так return this.age, контекст this CORRECT
+console.warn("2: " + user.age) // 20
+
+var getAge = user.getAge; // Указатель на функцию с глобальным контекстом
+console.warn("3: " + getAge()); // тут 10, так как глобальный контекст
+
+var getRealAge = getAge.bind(user); // присваеваем контекст - теперь контекст user
+console.warn("4: " + getRealAge()); // 20 - запрашиваем с user контекста
+console.warn("5: " + getAge()); // 10 - запрашиваем с глобального
+
+getRealAge = getAge.bind(); // создаем указатель вызова функции с глобальным контекстом
+console.warn("6: " + getRealAge()); // 10
