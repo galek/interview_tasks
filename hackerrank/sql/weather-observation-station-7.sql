7d36bac9170e65ff3b88106a190b28db3ff2a2d2
Nikolay Galko <galek@users.noreply.github.com>
2021-10-28 03:58:39 +0300

    Update weather-observation-station-7.sql

--- hackerrank/sql/weather-observation-station-7.sql
+++ hackerrank/sql/weather-observation-station-7.sql
@@ -1,4 +1,5 @@
 /*
 Enter your query here.
+https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
 */
 SELECT DISTINCT city FROM STATION WHERE city RLIKE '^.*[aeiouAEIOU]$'
