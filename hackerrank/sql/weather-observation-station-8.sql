dc6cdf86cb415c2d1291746632e4713bd395cb01
Nikolay Galko <galek@users.noreply.github.com>
2021-10-28 03:47:20 +0300

    Create weather-observation-station-8.sql

---
+++ hackerrank/sql/weather-observation-station-8.sql
@@ -0,0 +1,11 @@
+/*
+Enter your query here.
+*/
+
+/*
+LIKE - простое совпадение
+RLIKE - как регулярка
+https://oracleplsql.ru/rlike-mariadb.html
+*/
+
+SELECT DISTINCT city FROM STATION WHERE city LIKE '^[aeiouAEIOU].*[aeiouAEIOU]$'
