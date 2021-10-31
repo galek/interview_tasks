6c5d11a7c0a24ba5eb08d410f7f821b5ac88b115
Nikolay Galko <galek@users.noreply.github.com>
2021-10-31 05:26:43 +0300

    Create bublesort.cpp

---
+++ cpp/bublesort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// сортировка методом пузырька, по возрастанию
+// see std::sort(c++) quicksort(c) for reference
+// 
+// 1 2 3 
+// 3 1 2
+// 3 2 1
+
+void ResPrintf(const std::vector<int>& _v);
+
+
+void puz(std::vector<int> _v)
+{
+
+	for (size_t i = 0; i < _v.size(); i++) // don't forget about this!
+		for (size_t i = 1; i < _v.size(); i++)
+		{
+			if (_v[i] < _v[i - 1])
+			{
+				std::swap(_v[i], _v[i - 1]); // псевдокод, меняет местами 
+			}
+		}
+
+	//ResPrintf(_v);
+}
+
+void ResPrintf(const std::vector<int>& _v)
+{
+	for (auto c : _v)
+	{
+		std::cout << c << "\n";
+	}
+}
+
+void main()
+{
+	std::vector<int> a{ 3,2,1 };
+	puz(a);
+
+	// out 1,2,3 - correct
+
+	// quicksort
+	//std::sort(a.begin(), a.end());
+	//ResPrintf(a);
+
+	//system("pause");
+}
